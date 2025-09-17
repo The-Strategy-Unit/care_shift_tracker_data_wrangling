49bf0c938d858a3125f039ccba18c5b54de4f8bd
anyaferguson <anya.ferguson@nhs.net>
2025-09-17 09:29:01 +0100

    adding ccg to icb join

--- R/functions.R
+++ R/functions.R
@@ -2,6 +2,8 @@
 #'
 #' @param geography The geography column to group the data by.
 #' @param connection The ODBC connection.
+#' @param age An integer for the age cutoff.
+#' @param start The minimum date for the query.
 #'
 #' @returns A dataframe with the elective to non-elective admissions ratio by
 #' month and geography.
@@ -9,6 +11,8 @@ get_elective_non_elective_ratio <- function(geography,
                                             age = age_cutoff,
                                             start = start_date,
                                             connection = con) {
+  ccg_to_icb_join <- join_to_map_ccg_to_icb(geography)
+  
   query <- "
     SELECT
       convert(varchar(7), Discharge_Date, 120) AS date,
@@ -16,7 +20,9 @@ get_elective_non_elective_ratio <- function(geography,
       SUM(CASE WHEN Admission_Method IN ('11', '12', '13') THEN 1 ELSE 0 END) AS elective,
       SUM(CASE WHEN Admission_Method LIKE '2%' THEN 1 ELSE 0 END) AS non_elective
 
-    FROM Reporting_MESH_APC.APCE_Core_Monthly_Snapshot
+    FROM Reporting_MESH_APC.APCE_Core_Monthly_Snapshot AS apce
+      
+    ccg_to_icb_join
 
     WHERE Last_Episode_In_Spell_Indicator = '1'
       AND Discharge_Date >= 'start_date'
@@ -27,7 +33,8 @@ get_elective_non_elective_ratio <- function(geography,
     stringr::str_replace_all(c(
       "geography" = geography,
       "age_cutoff" = age,
-      "start_date" = start
+      "start_date" = start,
+      "ccg_to_icb_join" = ccg_to_icb_join
     ))
   
   wrangled <- DBI::dbGetQuery(connection, query) |>
@@ -44,4 +51,16 @@ get_elective_non_elective_ratio <- function(geography,
                   value)
 
   return(wrangled)
+}
+
+join_to_map_ccg_to_icb <- function(geography){
+  
+  join <- if(stringr::str_detect(geography, "icb")){
+    "LEFT JOIN [Internal_Reference].[CCGToICB_1] AS ref
+      ON apce.der_postcode_ccg_code = ref.org_code"
+  } else {
+    ""
+  }
+  
+  return(join)
 }
